84096957630f8bfff59ed6dd61166b427beb3c7e
Carlos Neto <51804831+carlosmcnt@users.noreply.github.com>
2022-08-13 20:36:19 -0300

    jogo de tiro ao alvo completo

---
+++ ShootingGallery/main.lua
@@ -0,0 +1,82 @@
+function love.load()
+    target = {}
+    target.x = 300
+    target.y = 300
+    target.radius = 50
+
+    score = 0
+    time = 0
+    gameState = 1
+
+    gameFont = love.graphics.newFont(30)
+
+    sprites = {}
+    sprites.sky = love.graphics.newImage('sprites/sky.png')
+    sprites.crosshairs = love.graphics.newImage('sprites/crosshairs.png')
+    sprites.target = love.graphics.newImage('sprites/target.png')
+
+    love.mouse.setVisible(false)
+end
+
+function love.update(dt)
+    if time > 0 then
+        time = time - dt
+    end
+    if time < 0 then
+        time = 0
+        gameState = 1
+    end
+end    
+
+function love.draw()
+    love.graphics.draw(sprites.sky, 0, 0)
+
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.setFont(gameFont)
+    love.graphics.print("Score: " .. score, 5, 5)
+    love.graphics.print("Time: " .. math.ceil(time), 300, 5)
+
+    if gameState == 1 then
+        love.graphics.printf("Click anywhere to begin!", 0, 250, love.graphics.getWidth(), "center")
+
+    elseif gameState == 2 then
+        love.graphics.draw(sprites.target, target.x-target.radius, target.y-target.radius)
+
+    end
+
+    love.graphics.draw(sprites.crosshairs, love.mouse.getX()-20, love.mouse.getY()-20)
+    
+end
+
+function love.mousepressed(x, y, button)
+    if gameState == 2 then
+
+        local mouseToTarget = distanceBetween(x, y, target.x, target.y)
+
+        if mouseToTarget < target.radius then
+            score = score + 1
+            if button == 1 then
+            	score = score + 1
+            elseif button == 2 then
+            	score = score + 2
+            	time = time - 1
+            end
+            target.x = math.random(target.radius, love.graphics.getWidth() - target.radius)
+            target.y = math.random(target.radius, love.graphics.getHeight() - target.radius)
+
+        elseif score > 0 then
+        	score = score - 1
+
+        end
+
+    elseif button == 1 and gameState == 1 then
+        gameState = 2
+        time = 20
+        score = 0
+        
+    end
+end
+
+function distanceBetween(x1, y1, x2, y2)
+    return math.sqrt((x2 - x1)^2 + (y2 - y1)^2)
+end
